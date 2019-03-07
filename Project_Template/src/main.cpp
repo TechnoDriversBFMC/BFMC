862ee801e2f47fe2663464a0f2166aff3637956b
Nandor Kilyen <Nandor.Kilyen2@ro.bosch.com>
2019-03-07 09:16:44 +0200

    Add a template Nucleo Project for Nucleo_F401RE

---
+++ Project_Template/src/main.cpp
@@ -0,0 +1,23 @@
+/**
+  ******************************************************************************
+  * @file    main.cpp
+  * @author  RBRO/PJ-IU
+  * @version V1.0.0
+  * @date    day-month-year
+  * @brief   Application entry point.
+  ******************************************************************************
+ */
+
+/* The mbed library */
+#include <mbed.h>
+
+
+/**
+ * @brief Main function
+ * 
+ * @return int 
+ */
+int main() 
+{
+    return 0;
+}
